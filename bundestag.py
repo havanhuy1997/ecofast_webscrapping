7942c093cf9d3947b2ca9304ba5553adc47ad18f
HaVanHuy <havanhuy1997@gmail.com>
2020-03-12 21:23:11 +0300

    add extract pdf file

--- bundestag.py
+++ bundestag.py
@@ -1,6 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
-from file import create_folder_if_not_existing
+from file import create_folder_if_not_existing, get_pdf_from_link
 from config import OUTPUT_DIR
 import re
 
@@ -40,19 +40,22 @@ for gtyp in gtyps:
             a_tag = tr.select_one('.resultTitle a')
             href = a_tag.attrs['href']
             title = a_tag.text
-            full_content = tr.select_one('.fullCont').text
+            pdf_doc = get_pdf_from_link(href)
             date = tr.select('.resultText strong')[1].text
             date_for_title = get_date_for_title(date)
 
             output_dir = OUTPUT_DIR + '{}_DE_BUNDESTAG'.format(date_for_title)
             create_folder_if_not_existing(output_dir)
             file_name = '{}_DE_BUNDESTAG_{}.txt'.format(date_for_title, re.sub('[^0-9a-zA-Z]', '', title))
-            with open(output_dir + '/' + file_name, 'w') as f:
+            with open(output_dir + '/' + file_name, 'w', encoding="utf-8") as f:
                 print('+ Saving {}'.format(title))
                 f.write(date + '\n')
                 f.write(title + '\n')
                 f.write(href + '\n')
-                f.write(full_content)
+                if type(pdf_doc) == str:
+                    f.write(pdf_doc)
+                else:
+                    f.writelines(pdf_doc)
         if exist_next_page(soup):
             h += RESULTS_PER_PAGE
         else:
--- file.py
+++ file.py
@@ -1,5 +1,29 @@
 import os
+import requests
+from io import BytesIO
+import PyPDF2
+import slate3k as slate
 
 def create_folder_if_not_existing(folder_path):
     if not os.path.exists(folder_path):
         os.mkdir(folder_path)
+
+def get_pdf_from_link(link):
+    print('Getting pdf content:', link)
+    content = ''
+    r = requests.get(link)
+    file_bytes = BytesIO(r.content)
+    pdf_reader = PyPDF2.PdfFileReader(file_bytes)
+    for i in range(pdf_reader.getNumPages()):
+        try:
+            page = pdf_reader.getPage(i)
+            text = page.extractText()
+            if i == 0 and text.strip() and ' ' not in text:
+                break
+            content += page.extractText()
+        except Exception as e:
+            print('Error {} when getting content for page: {}'.format(str(e), i))
+    if not content:
+        file_bytes = BytesIO(r.content)
+        content = slate.PDF(file_bytes)
+    return content
